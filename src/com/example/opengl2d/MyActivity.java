119fb7f0b916cc12d318261ac233b264c442af1b
Avent08 <andhika.ventausa@gmail.com>
2012-11-30 16:39:22 +0700

    test new commit

---
+++ src/com/example/opengl2d/MyActivity.java
@@ -0,0 +1,34 @@
+package com.example.opengl2d;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MyActivity extends Activity
+{
+    private GLSurfaceView mGLSurfaceView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+       // Remove the title bar from the window.
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// Make the windows into full screen mode.
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // Create an OpenGL view.
+        mGLSurfaceView = new GLSurfaceView(this);
+        OpenGLRenderer renderer = new OpenGLRenderer();
+		mGLSurfaceView.setRenderer(renderer);
+		setContentView(mGLSurfaceView);
+    }
+}
+
